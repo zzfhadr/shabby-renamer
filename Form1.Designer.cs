3f61495963f951dc78397a95089fc609f28a0e3f
LOOMING <zzfhadr@gmail.com>
2024-04-04 10:43:21 +0800

    back up my code

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace rename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.40904F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.59096F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(907, 534);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.button3);
+            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.button5);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 270);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(423, 261);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(421, 271);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "LOAD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(3, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(84, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(165, 280);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(246, 280);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.button6);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(432, 270);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(471, 261);
+            this.flowLayoutPanel2.TabIndex = 1;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(3, 3);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(468, 258);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "CHANGE";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(423, 261);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(432, 3);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(463, 261);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = resources.GetString("textBox2.Text");
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 534);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Renamer";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Security;
+using System.Security.Permissions;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+using System.IO;
+
+namespace rename
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//load button way to use : change . in music title to 丶
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Songs (*.MP3;*.FLAC;*.WAV)|*.MP3;*.FLAC;*.WAV|" + 
+                "All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (String filePath in openFileDialog1.FileNames)
+                {
+                    // Create a PictureBox.
+                    try
+                    {
+                        //string filePath = @"D:\desktop\LOOMING\Desktop\qwer\0.序章.mp3";
+                        var file = ShellFile.FromFilePath(filePath);
+
+                        // Read and Write:
+
+                        string[] oldAuthors = file.Properties.System.Author.Value;
+                        string oldTitle = file.Properties.System.Title.Value;
+                        StringBuilder newTile = new StringBuilder(3650);
+                        //file.Properties.System.Author.Value = new string[] { "Author #1", "Author #2" };
+                        if (oldTitle == null || oldTitle == "kuwo")
+                        //if (true)
+                        {
+
+                            //get file name
+                            int i, j;
+                            StringBuilder filename = new StringBuilder(3650);
+                            // MessageBox.Show("filepath:"+filePath);
+  
+                            j = 0;
+                            for (i = filePath.Length - 1; i >= 0; i--)
+                            {
+                                //there is a problem here!!!
+                                if (filePath[i] == '\\' && j == 0)
+                                    j = 1;
+                                // MessageBox.Show(" "+filePath[i]);
+                                if (j == 0)
+                                    filename.Append(filePath[i]);
+
+
+                            }
+                            oldTitle = new string (filename.ToString().Reverse().ToArray());
+                        }
+                        //int counter = -1;
+                            foreach (char caca in oldTitle)
+                            {
+                               // counter++;
+
+                                if (caca == '.')
+                                    newTile.Append('、');
+                                else
+                                    newTile.Append(caca);
+
+                            }
+                            file.Properties.System.Title.Value = newTile.ToString();
+                        
+                       
+
+                        // Alternate way to Write:
+
+                        //ShellPropertyWriter propertyWriter = file.Properties.GetPropertyWriter();
+                        //propertyWriter.WriteProperty(SystemProperties.System.Author, new string[] { "Author" });
+                        //propertyWriter.Close();
+                    }
+                    catch (SecurityException ex)
+                    {
+                        // The user lacks appropriate permissions to read files, discover paths, etc.
+                        MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+                            "Error message: " + ex.Message + "\n\n" +
+                            "Details (send to Support):\n\n" + ex.StackTrace
+                        );
+                    }
+                    catch (Exception ex)
+                    {
+                        // Could not load the image - probably related to Windows file system permissions.
+                        MessageBox.Show("Cannot display the image: " + filePath.Substring(filePath.LastIndexOf('\\'))
+                            + ". You may not have permission to read the file, or " +
+                            "it may be corrupt.\n\nReported error: " + ex.Message);
+                    }
+                }
+            }
+
+
+
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)//change buttion way to use : generete irc for 
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Songs (*.MP3;*.FLAC;*.WAV)|*.MP3;*.FLAC;*.WAV|" +
+                "All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Multiselect = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (String filePath in openFileDialog1.FileNames)
+                {
+                    // Create a PictureBox.
+                    try
+                    {
+
+                        //get file name
+                        int i, j;
+                        StringBuilder filename = new StringBuilder(3650);
+                        // MessageBox.Show("filepath:"+filePath);
+                        StringBuilder pathpath = new StringBuilder(3560);
+                        j = 0;
+                        for (i = filePath.Length - 1; i >= 0; i--)
+                        {
+                            //there is a problem here!!!
+                            if (filePath[i] == '\\'&& j == 0 )
+                                j=1;
+                            // MessageBox.Show(" "+filePath[i]);
+                            if (j == 0)
+                                filename.Append(filePath[i]);
+                            else
+                                pathpath.Append(filePath[i]);
+
+                        }
+
+
+                        //get rid of .mp3
+                        StringBuilder lrcfile = new StringBuilder(3600);
+                        i = 231;
+                        foreach (char c9 in filename.ToString())
+                        {
+                            if (c9 == '.' && i == 231)
+                            {
+                                i = 0;
+                            }
+                            if(i==0)
+                            {
+                                lrcfile.Append(c9);
+                                //i++;
+                            }
+                        }
+                        //plus .lrc and path 
+                        //FINALly name got!!!!
+
+                        lrcfile.Remove(0, 1);
+                        string lrcfullname = new string(pathpath.ToString().Reverse().ToArray())+ new string(lrcfile.ToString().Reverse().ToArray()) + ".lrc";
+                        // Create a FileInfo  
+
+                        //rename
+                        System.IO.FileInfo fi = new System.IO.FileInfo(lrcfullname);
+                        // Check if file is there  
+                        if (fi.Exists)
+                        {
+                            var file = ShellFile.FromFilePath(filePath);
+                            string[] oldAuthors = file.Properties.System.Author.Value;
+                            string oldTitle = file.Properties.System.Title.Value;
+                            // Move file with a new name. Hence renamed.
+                            
+                            Directory.CreateDirectory(new string(pathpath.ToString().Reverse().ToArray()) + @"ライン");
+                            if (oldAuthors[0] == null)
+                            { 
+                                ShellPropertyWriter propertyWriter = file.Properties.GetPropertyWriter();
+                                propertyWriter.WriteProperty(SystemProperties.System.Author, new string[] { "Ace" });
+                                propertyWriter.Close();
+                                 oldAuthors = file.Properties.System.Author.Value;
+                            }
+
+                            if (oldTitle == null)
+                            {
+                                
+                                fi.CopyTo(new string(pathpath.ToString().Reverse().ToArray()) + @"ライン\" + oldAuthors[0] + " - " + new string (lrcfile.ToString().Reverse().ToArray()) + ".lrc");
+                                
+                            }
+                            else
+                            fi.CopyTo(new string(pathpath.ToString().Reverse().ToArray())+ @"ライン\" + oldAuthors[0] + " - " + oldTitle+".lrc");
+                            //Console.WriteLine("File Renamed.");
+                        }
+                        else
+                            MessageBox.Show(lrcfullname);
+
+                        // MessageBox.Show("lrcfullname:" + lrcfullname);
+                        //Console.WriteLine(lrcfullname);
+
+                        //string filePath = @"D:\desktop\LOOMING\Desktop\qwer\0.序章.mp3";
+
+                    }
+                    catch (SecurityException ex)
+                    {
+                        // The user lacks appropriate permissions to read files, discover paths, etc.
+                        MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+                            "Error message: " + ex.Message + "\n\n" +
+                            "Details (send to Support):\n\n" + ex.StackTrace
+                        );
+                    }
+                    catch (Exception ex)
+                    {
+                        // Could not load the image - probably related to Windows file system permissions.
+                        MessageBox.Show("Cannot display the image: " + filePath.Substring(filePath.LastIndexOf('\\'))
+                            + ". You may not have permission to read the file, or " +
+                            "it may be corrupt.\n\nReported error: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
+
